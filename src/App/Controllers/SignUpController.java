b7233197dc2d7a7a67846a790b2c9ebeb7c0de33
Krzysztof <krzysztofjonczyk121@gmail.com>
2019-03-16 23:55:53 +0100

    Working on restrictions on signup

--- src/App/Controllers/SignUpController.java
+++ src/App/Controllers/SignUpController.java
@@ -1,6 +1,7 @@
 package App.Controllers;
 
 import App.Main;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -8,10 +9,12 @@ import javafx.scene.Parent;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ResourceBundle;
@@ -19,14 +22,16 @@ import java.util.ResourceBundle;
 public class SignUpController implements Initializable {
 
     @FXML
-    void btn_close(MouseEvent event) {
-        System.exit(0);
-    }
+    private FontAwesomeIconView addedProperly;
 
     @FXML
-    void btn_minimize(MouseEvent event){
-        universalMethods.minimize(event);
-    }
+    private Text fillAll, emailExists, userExists;
+
+    @FXML
+    void btn_close(MouseEvent event) { System.exit(0); }
+
+    @FXML
+    void btn_minimize(MouseEvent event){ universalMethods.minimize(event); }
 
     @FXML
     void login(MouseEvent event) throws IOException {
@@ -39,22 +44,41 @@ public class SignUpController implements Initializable {
 
     @FXML
     void signup(MouseEvent event) throws SQLException {
-        String jdbcUrl = "jdbc:mysql://localhost:3306/user_tracker?useSSL=false&serverTimezone=UTC";
 
-        String username = tf_username.getText();
-        String email= tf_email.getText();
-        String password = pf_password.getText();
+        addedProperly.setVisible(false);
+        fillAll.setVisible(false);
+        userExists.setVisible(false);
+        emailExists.setVisible(false);
+
+        boolean checkEmail = universalMethods.isValidEmailAddress(tf_email.getText());
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/user_tracker?useSSL=false&serverTimezone=UTC";
 
         Connection myConn = DbConnect.getInstance().getConnection(jdbcUrl);
 
         Statement statement = myConn.createStatement();
 
-        int status = (statement).executeUpdate("insert into user (username,email,password)" + " values('"+ username +"', '"+ email +"', '" + password + "')");
+        ResultSet resultName = statement.executeQuery("select * from user where binary username" +
+                " =  '" + tf_username.getText() + "'" );
+        if (!resultName.next()) {
+            
+            ResultSet resultEmail = statement.executeQuery("select * from user where binary email" + " =  " +
+                    "'" + tf_email.getText() + "'");
 
+            if (!resultEmail.next()) {
 
-        if(status > 0) {
-            System.out.println("user registered");
-        }
+                int status = (statement).executeUpdate("insert into user (username,email,password)" + "" +
+                        "values('" + tf_username.getText() + "', '" + tf_email.getText() + "', '" + pf_password.getText() + "')");
+
+                if (status > 0) {
+                    addedProperly.setVisible(true);
+                    fillAll.setVisible(false);
+                    universalMethods.clearAllFields(tf_username, tf_email, pf_password, null,null,null); //Not sure if it's a good practice
+                    System.out.println("user registered");
+                }
+            } else emailExists.setVisible(true);
+
+        }else userExists.setVisible(true);
     }
 
     @Override
